6361268c04215c1bd038d7e4d6bb9303c75a5617
gadmat <matej.gadzo5@gmail.com>
2021-10-21 13:07:09 +0200

    cleanup

--- src/app/app.state.ts
+++
@@ -1,9 +0,0 @@
-import { PostModel } from "../app/models/post.model";
-import { CommentModel } from "./models/comment.model";
-import { UserModel } from "./models/user.model";
-
-export interface AppState{
-  readonly posts: PostModel;
-  readonly users: UserModel[];
-  readonly comments: CommentModel[];
-}
